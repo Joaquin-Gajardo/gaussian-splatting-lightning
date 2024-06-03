07e283feef039adc6516527754b6a6e3c4cd8bc7
Joaquin-Gajardo <joaquin.gajardocastillo@gmail.com>
2024-06-03 18:38:31 +0200

    add another date to demoanlage viewer script

--- run_viewer_demoanlage.sh
+++ run_viewer_demoanlage.sh
@@ -8,8 +8,10 @@ export CUDA_VISIBLE_DEVICES=$gpu; python viewer.py -p $port \
     ../../output/demoanlage/field_C/20240322/point_cloud/iteration_30000/point_cloud.ply \
     ../../output/demoanlage/field_C/20240328/point_cloud/iteration_30000/point_cloud.ply \
     ../../output/demoanlage/field_C/20240405/point_cloud/iteration_30000/point_cloud.ply \
-    ../../output/demoanlage/field_C/20240411/point_cloud/iteration_30000/point_cloud.ply \
     ../../output/demoanlage/field_C/20240416/point_cloud/iteration_30000/point_cloud.ply \
     ../../output/demoanlage/field_C/20240425/point_cloud/iteration_30000/point_cloud.ply \
     ../../output/demoanlage/field_C/20240508/point_cloud/iteration_30000/point_cloud.ply \
-    ../../output/demoanlage/field_C/20240513/point_cloud/iteration_30000/point_cloud.ply
+    ../../output/demoanlage/field_C/20240528/point_cloud/iteration_7000/point_cloud.ply
+    
+    #../../output/demoanlage/field_C/20240411/point_cloud/iteration_30000/point_cloud.ply \
+    #../../output/demoanlage/field_C/20240513/point_cloud/iteration_30000/point_cloud.ply \ 
